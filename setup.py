053c0e3da0c522a963b754a760aab78ce4fbd1df
shiinokinoki <shue.go.go@gmail.com>
2025-02-05 19:39:56 +0900

    .

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nokinoki-test111",  # パッケージ名
-    version="0.1.2",  # バージョン
+    name="spreadsheet-intelligence",  # パッケージ名
+    version="0.1.0",  # バージョン
     author="Galirage, Inc.",
     author_email="info@galirage.com",
     description="A package for spreadsheet data loader to process with LLM",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/galirage/gg-knowledge-base",  # GitHubリポジトリなど
+    url="https://github.com/galirage/spreadsheet-intelligence",  # GitHubリポジトリなど
     packages=find_packages(exclude=["tests*", "tests"]),
     include_package_data=True,
     install_requires=["matplotlib"],
